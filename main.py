8e0b46406154fe3711dc17fe6b25c36c6c3f142c
passdacom <passcom.net@gmail.com>
2025-08-12 09:59:31 +0900

    Revert "feat: 관리자 권한 자동 요청 기능 추가"
    This reverts commit cb3edd8a1afee196a73de9eb2e2db64fdd507603.

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 
 import tkinter as tk
-import ctypes
-import sys
 from app_gui import AppGUI
 
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
 if __name__ == '__main__':
-    if is_admin():
-        # 관리자 권한으로 실행된 경우, 메인 애플리케이션 실행
-        root = tk.Tk()
-        app = AppGUI(root)
-        root.mainloop()
-    else:
-        # 관리자 권한이 없는 경우, 권한 상승을 요청하며 재실행
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    root = tk.Tk()
+    app = AppGUI(root)
+    root.mainloop()
